5fb7a24d64607659034ca055ef0c0d14f24baa43
unknown <unknown@example.com>
2024-06-16 00:11:32 +0800

    add translate

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const redis = require('redis');
+const {MET} = require("bing-translate-api");
 
 const REDIS_CONNECTION_STRING = process.env.REDIS_CONNECTION_STRING;
 const AUTH_API_KEY = process.env.AUTH_API_KEY;
@@ -35,9 +36,9 @@ app.get('/base/:key', async (req, res) => {
     const key = req.params.key;
     try {
         const value = await client.get(key);
-        res.send(value);
+        res.contentType('text/plain').send(value);
     } catch (err) {
-        res.status(500).send(err.message);
+        res.status(500).contentType('text/plain').send(err.message);
     }
 });
 // PUT 保存内容，如果key已经存在则返回409
@@ -48,12 +49,12 @@ app.put('/base/:key', async (req, res) => {
     try {
         const result = expires === undefined ? await client.setnx(key, value) : await client.set(key, value, 'EX', expires);
         if (result === 0) {
-            res.status(409).send('Key already exists');
+            res.status(409).contentType('text/plain').send('Key already exists');
         } else {
-            res.send('1');
+            res.status(201).contentType('text/plain').send('1');
         }
     } catch (err) {
-        res.status(500).send(err.message);
+        res.status(500).contentType('text/plain').send(err.message);
     }
 });
 // POST 保存内容，已经存在则覆盖
@@ -68,9 +69,9 @@ app.post('/base/:key', async (req, res) => {
         } else {
             result = await client.set(key, value, 'EX', expires);
         }
-        res.send(result === 'OK' ? '1' : '0');
+        res.contentType('text/plain').send(result === 'OK' ? '1' : '0');
     } catch (err) {
-        res.status(500).send(err.message);
+        res.status(500).contentType('text/plain').send(err.message);
     }
 });
 // DELETE 删除内容
@@ -79,13 +80,44 @@ app.delete('/base/:key', async (req, res) => {
     try {
         const result = await client.del(key);
         if (result === 0) {
-            res.status(404).send('Key not found');
+            res.status(404).contentType('text/plain').send('Key not found');
         } else {
-            res.send('Key deleted successfully');
+            res.contentType('text/plain').send('1');
         }
     } catch (err) {
-        res.status(500).send(err.message);
+        res.status(500).contentType('text/plain').send(err.message);
     }
+});
+
+// 必应翻译
+const __translate = async (req, res) => {
+    let text = req.query.text
+    if (!text && req.body) {
+        text = req.body;
+    }
+    let from = req.query.from;
+    let to = req.query.to;
+    let asHtml = req.query.asHtml;
+    const isEnglish = /^[a-zA-Z]+$/.test(text);
+    if (!from && !to) {
+        from = isEnglish ? 'en' : null;
+        to = isEnglish ? 'zh-Hans' : 'en';
+    }
+    MET.translate(text, from, to, {
+        translateOptions: {
+            textType: asHtml ? 'html' : 'plain',
+        }
+    }).then(result => {
+        res.status(200).contentType('text/plain').send(result[0].translations[0].text);
+    }).catch(err => {
+        res.status(500).contentType('text/plain').send(err);
+    });
+};
+app.post("/tools/bing-translate", __translate);
+app.get("/tools/bing-translate", __translate);
+
+app.get('/tools/bing-translate/languages', async (req, res) => {
+    res.status(200).json(MET.lang.LANGS);
 });
 
 
